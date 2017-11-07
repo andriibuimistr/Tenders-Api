9b223a59e9df3beb3c903823f043a1b0e58ed2aa
Andrii <andriibuimistr@gmail.com>
2017-11-07 23:20:52 +0200

    Commit 07.11.2017

--- index.py
+++ index.py
@@ -8,61 +8,53 @@ import sys
 import qualification
 
 
-procurement_method = variables.procurement_method_selector()
-if procurement_method in variables.above_threshold_procurement:
-    number_of_lots = variables.number_of_lots()
-    if number_of_lots == 0:
-        number_of_items = variables.number_of_items()
-    else:
-        number_of_items = 0
-elif procurement_method in variables.below_threshold_procurement:
-    sys.exit("Error. Данный функционал еще не был разработан :)")
-else:
-    sys.exit("Error. Данный функционал еще не был разработан :)")
-
-number_of_bids = bid.number_of_bids()
-
-
-#procurement_method = 'aboveThresholdUA'
-#number_of_lots = 0
-#number_of_items = 2
+# procurement_method = 'aboveThresholdUA'
+# number_of_lots = 0
+# number_of_items = 2
 add_documents = 0
-#number_of_bids = 1
+# number_of_bids = 1
 
 
-list_of_id_lots = tender.list_of_id_for_lots(number_of_lots)  # get list of id for lots
+def create_tender_function():
 
-# select type of tender (with or without lots)
-if number_of_lots == 0:
-    json_tender = json.loads(tender.tender(number_of_lots, number_of_items, procurement_method))
-else:
-    json_tender = json.loads(tender.tender_with_lots(number_of_lots, number_of_items, list_of_id_lots,
-                                                     procurement_method))
+    procurement_method = variables.procurement_method_selector()
+    if procurement_method in variables.above_threshold_procurement:
+        number_of_lots = variables.number_of_lots()
+        if number_of_lots == 0:
+            number_of_items = variables.number_of_items()
+        else:
+            number_of_items = 0
+    elif procurement_method in variables.below_threshold_procurement:
+        sys.exit("Error. Данный функционал еще не был разработан :)")
+    else:
+        sys.exit("Error. Данный функционал еще не был разработан :)")
+    number_of_bids = bid.number_of_bids()
 
-headers_tender = tender.headers_tender(json_tender)  # get headers for publish tender
+    list_of_id_lots = tender.list_of_id_for_lots(number_of_lots)  # get list of id for lots
+    # select type of tender (with or without lots)
+    if number_of_lots == 0:
+        json_tender = json.loads(tender.tender(number_of_lots, number_of_items, procurement_method))
+    else:
+        json_tender = json.loads(tender.tender_with_lots(number_of_lots, number_of_items, list_of_id_lots,
+                                                         procurement_method))
+    headers_tender = tender.headers_tender(json_tender)  # get headers for publish tender
+    publish_tender_response = tender.publish_tender(headers_tender, json_tender)  # publish tender in draft status
+    activate_tender = tender.activating_tender(publish_tender_response, headers_tender)  # activate tender
 
-publish_tender_response = tender.publish_tender(headers_tender, json_tender)  # publish tender in draft status
-activate_tender = tender.activating_tender(publish_tender_response, headers_tender)  # activate tender
+    tender_id_long = publish_tender_response.headers['Location'].split('/')[-1]
+    tender_token = publish_tender_response.json()['access']['token']
+    tender_status = activate_tender.json()['data']['status']
 
-tender_id_long = publish_tender_response.headers['Location'].split('/')[-1]
-tender_token = publish_tender_response.json()['access']['token']
-tender_status = activate_tender.json()['data']['status']
-tender_id_short = publish_tender_response.json()['data']['tenderID']
-
-
-# add documents to tender
-def add_docs():
+    # add documents to tender
     if add_documents == 1:
         document.add_documents_to_tender(tender_id_long, tender_token)
-add_docs()
-# add tender to database
-add_tender_to_db = tender.tender_to_db(tender_id_long, publish_tender_response, tender_token, procurement_method,
-                                       tender_status, number_of_lots)
-
-# add_tender_to_site = tender.add_tender_to_site(tender_id_long, tender_token)
-
-# BIDS
-bid.run_cycle(number_of_bids, number_of_lots, tender_id_long, procurement_method, list_of_id_lots)
+    # add tender to database
+    tender.tender_to_db(tender_id_long, publish_tender_response, tender_token, procurement_method, tender_status,
+                        number_of_lots)
+    # tender.add_tender_to_site(tender_id_long, tender_token)
+    # bids
+    bid.run_cycle(number_of_bids, number_of_lots, tender_id_long, procurement_method, list_of_id_lots)
+# create_tender_function()
 
 
 # ########################## APPROVE PREQUALIFICATION ###################################
@@ -72,4 +64,4 @@ def pass_prequalification():
     qualifications = qualification.list_of_qualifications(tender_id_long)  # get list of qualifications for tender
     qualification.approve_my_bids(qualifications, tender_id_long, tender_token)  # approve all my bids
     qualification.finish_prequalification(tender_id_long, tender_token)  # submit prequalification protocol
-# pass_prequalification()
+pass_prequalification()
