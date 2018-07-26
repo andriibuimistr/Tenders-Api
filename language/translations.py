93ad80087173178a1c5926f13d1749e300facc1f
Andrey <andriibuimistr@gmail.com>
2018-07-26 17:30:00 +0300

    Add translations for creation forms

--- language/translations.py
+++ language/translations.py
@@ -31,6 +31,24 @@ class RU(object):
     page_title_tenders_management = u'Управление закупками'
     page_title_user_preferences = u'Настройки пользователя'
 
+    form_placeholder_number_of_lots = u'Кол-во лотов'
+    form_placeholder_number_of_items = u'Кол-во предметов'
+    form_placeholder_number_of_bids = u'Кол-во ставок'
+    form_placeholder_accelerator = u'Ускорение'
+    form_placeholder_company_id = u'ID компании'
+    form_checkbox_features = u'Неценовые'
+    form_checkbox_skip_auction = u'Без аукциона'
+    form_checkbox_documents_for_tender = u'Документы к тендеру'
+    form_checkbox_documents_for_bids = u'Документы к ставке'
+    form_checkbox_rent = u'Аренда'
+    form_checkbox_one_bid = u'Одна ставка'
+
+    button_create_tender = u'Создать закупку'
+    button_create_auction = u'Создать аукцион'
+    button_delete_alerts = u'Удалить сообщения'
+
+    section_create_tender_response = u'Результаты запросов'
+
 
 class EN(object):
     def __init__(self):
@@ -61,6 +79,24 @@ class EN(object):
     page_title_tenders_management = u'Tenders management'
     page_title_user_preferences = u'User Settings'
 
+    form_placeholder_number_of_lots = u'Number of lots'
+    form_placeholder_number_of_items = u'Number of items'
+    form_placeholder_number_of_bids = u'Number of bids'
+    form_placeholder_accelerator = u'Accelerator'
+    form_placeholder_company_id = u'Company ID'
+    form_checkbox_features = u'Features'
+    form_checkbox_skip_auction = u'Skip auction'
+    form_checkbox_documents_for_tender = u'Documents for tender'
+    form_checkbox_documents_for_bids = u'Documents for bids'
+    form_checkbox_rent = u'Rent'
+    form_checkbox_one_bid = u'One bid'
+
+    button_create_tender = u'Create tender'
+    button_create_auction = u'Create auction'
+    button_delete_alerts = u'Delete messages'
+
+    section_create_tender_response = u'Requests results'
+
 
 class ES(object):
     def __init__(self):
@@ -90,6 +126,24 @@ class ES(object):
     page_title_users_management = u'Gestión de usuarios'
     page_title_tenders_management = u'Gestión de licitaciones'
     page_title_user_preferences = u'Configuración del usuario'
+
+    form_placeholder_number_of_lots = u'Núm. de lotes'
+    form_placeholder_number_of_items = u'Núm. de artículos'
+    form_placeholder_number_of_bids = u'Núm. de apuestas'
+    form_placeholder_accelerator = u'Acelerador'
+    form_placeholder_company_id = u'ID de compañía'
+    form_checkbox_features = u'Características'
+    form_checkbox_skip_auction = u'Saltar subasta'
+    form_checkbox_documents_for_tender = u'Documentos de licitación'
+    form_checkbox_documents_for_bids = u'Documentos de apuestas'
+    form_checkbox_rent = u'Alquiler'
+    form_checkbox_one_bid = u'Una apuesta'
+
+    button_create_tender = u'Crear licitación'
+    button_create_auction = u'Crear subasta'
+    button_delete_alerts = u'Eliminar mensajes'
+
+    section_create_tender_response = u'Resultados de las solicitudes'
 
 
 class Translations(object):
@@ -174,3 +228,51 @@ class Translations(object):
 
     def page_title_user_preferences(self):
         return self.class_selector().page_title_user_preferences
+
+    # FORMS ELEMENTS
+    def form_placeholder_number_of_lots(self):
+        return self.class_selector().form_placeholder_number_of_lots
+
+    def form_placeholder_number_of_items(self):
+        return self.class_selector().form_placeholder_number_of_items
+
+    def form_placeholder_number_of_bids(self):
+        return self.class_selector().form_placeholder_number_of_bids
+
+    def form_placeholder_accelerator(self):
+        return self.class_selector().form_placeholder_accelerator
+
+    def form_placeholder_company_id(self):
+        return self.class_selector().form_placeholder_company_id
+
+    def form_checkbox_features(self):
+        return self.class_selector().form_checkbox_features
+
+    def form_checkbox_skip_auction(self):
+        return self.class_selector().form_checkbox_skip_auction
+
+    def form_checkbox_documents_for_tender(self):
+        return self.class_selector().form_checkbox_documents_for_tender
+
+    def form_checkbox_documents_for_bids(self):
+        return self.class_selector().form_checkbox_documents_for_bids
+
+    def form_checkbox_rent(self):
+        return self.class_selector().form_checkbox_rent
+
+    def form_checkbox_one_bid(self):
+        return self.class_selector().form_checkbox_one_bid
+
+    # BUTTONS
+    def button_create_tender(self):
+        return self.class_selector().button_create_tender
+
+    def button_create_auction(self):
+        return self.class_selector().button_create_auction
+
+    def button_delete_alerts(self):
+        return self.class_selector().button_delete_alerts
+
+    # NAMES FOR SECTIONS
+    def section_create_tender_response(self):
+        return self.class_selector().section_create_tender_response
