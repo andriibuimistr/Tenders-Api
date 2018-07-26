086a0f17bb72264f0a2fd229019f35c8be0b9ae3
Andrey <andriibuimistr@gmail.com>
2018-07-26 20:25:10 +0300

    Add translations for admin pages

--- language/translations.py
+++ language/translations.py
@@ -39,12 +39,19 @@ class RU(object):
     form_placeholder_number_of_bids = u'Кол-во ставок'
     form_placeholder_accelerator = u'Ускорение'
     form_placeholder_company_id = u'ID компании'
+    form_placeholder_paste_json = u'Место для вставки JSON'
+    form_placeholder_platform_name = u'Имя площадки'
+    form_placeholder_platform_url = u'Адрес площадки'
+    form_placeholder_username = u'Имя пользователя'
+    form_placeholder_password = u'Пароль'
     form_checkbox_features = u'Неценовые'
     form_checkbox_skip_auction = u'Без аукциона'
     form_checkbox_documents_for_tender = u'Документы к тендеру'
     form_checkbox_documents_for_bids = u'Документы к ставке'
     form_checkbox_rent = u'Аренда'
     form_checkbox_one_bid = u'Одна ставка'
+    form_checkbox_insert_json = u'Вставить JSON'
+    form_checkbox_get_json_from_cdb = u'JSON из ЦБД'
 
     # Buttons
     button_create_tender = u'Создать закупку'
@@ -52,19 +59,44 @@ class RU(object):
     button_delete_alerts = u'Удалить сообщения'
     button_show_bids = u'Показать ставки'
     button_add_to_company = u'Добавить'
+    button_add = u'Добавить'
+    button_save = u'Сохранить'
+    button_format_json = u'Форматировать'
+    button_get_json = u'Получить JSON'
+    button_delete_tenders = u'Удалить закупки'
 
     # Sections names
     section_create_tender_response = u'Результаты запросов'
     section_list_of_bids = u'Ставки'
+    section_input_json = u'Оригинальный JSON'
+    section_output_json = u'Отформатированный JSON'
+    section_list_of_platforms = u'Список площадок'
+    section_list_of_users = u'Список пользователей'
+    section_list_of_tenders = u'Список Закупок'
+    section_popular_links = u'Популярные ссылки'
 
     # Labels
     label_company_identifier = u'Идентификатор комп-и'
     label_company_id = u'ID компании'
     label_platform = u'Площадка'
+    label_id_in_cdb = u'ID в ЦБД'
+    label_procurement_method_type = u'Тип процедуры'
+    label_number_of_lots = u'Кол-во лотов'
+    label_creator = u'Создатель'
+    label_api_version = u'Версия API'
+    label_status = u'Статус'
+    label_added_to_site = u'На сайте'
+    label_phone = u'Телефон'
 
     # Messages
     message_tender_has_no_bids = u'В базе данных нет ставок для данной закупки'
     message_auction_has_no_bids = u'В базе данных нет ставок для данной закупки'
+    message_rendered_json_will_appear_here = u'Отформатированный JSON появится здесь'
+    message_empty_list = u'Список пуст'
+    message_all_rights_reserved = u'Все права защищены'
+
+    # Hints
+    hint_delete = u'Удалить'
 
 
 class EN(object):
@@ -104,12 +136,19 @@ class EN(object):
     form_placeholder_number_of_bids = u'Number of bids'
     form_placeholder_accelerator = u'Accelerator'
     form_placeholder_company_id = u'Company ID'
+    form_placeholder_paste_json = u'Paste your JSON here'
+    form_placeholder_platform_name = u'Platform name'
+    form_placeholder_platform_url = u'Platform url'
+    form_placeholder_username = u'Username'
+    form_placeholder_password = u'Password'
     form_checkbox_features = u'Features'
     form_checkbox_skip_auction = u'Skip auction'
     form_checkbox_documents_for_tender = u'Documents for tender'
     form_checkbox_documents_for_bids = u'Documents for bids'
     form_checkbox_rent = u'Rent'
     form_checkbox_one_bid = u'One bid'
+    form_checkbox_insert_json = u'Insert JSON'
+    form_checkbox_get_json_from_cdb = u'Get JSON from CDB'
 
     # Buttons
     button_create_tender = u'Create tender'
@@ -117,19 +156,44 @@ class EN(object):
     button_delete_alerts = u'Delete messages'
     button_show_bids = u'Show bids'
     button_add_to_company = u'Add'
+    button_add = u'Add'
+    button_save = u'Save'
+    button_format_json = u'Format'
+    button_get_json = u'Get JSON'
+    button_delete_tenders = u'Delete tenders'
 
     # Sections names
     section_create_tender_response = u'Requests results'
     section_list_of_bids = u'Bids'
+    section_input_json = u'Original JSON'
+    section_output_json = u'Output JSON'
+    section_list_of_platforms = u'List of platforms'
+    section_list_of_users = u'List of users'
+    section_list_of_tenders = u'List of tenders'
+    section_popular_links = u'Popular links'
 
     # Labels
     label_company_identifier = u'Company identifier'
     label_company_id = u'Company ID'
     label_platform = u'Platform'
+    label_id_in_cdb = u'ID in CDB'
+    label_procurement_method_type = u'Type of procurement'
+    label_number_of_lots = u'Number of lots'
+    label_creator = u'Creator'
+    label_api_version = u'API version'
+    label_status = u'Status'
+    label_added_to_site = u'Added to site'
+    label_phone = u'Phone number'
 
     # Messages
     message_tender_has_no_bids = u'There are no bids for this tender in the database'
     message_auction_has_no_bids = u'There are no bids for this auction in the database'
+    message_rendered_json_will_appear_here = u'Formatted JSON will appear here'
+    message_empty_list = u'List is empty'
+    message_all_rights_reserved = u'All rights reserved'
+
+    # Hints
+    hint_delete = u'Delete'
 
 
 class ES(object):
@@ -169,12 +233,19 @@ class ES(object):
     form_placeholder_number_of_bids = u'Núm. de apuestas'
     form_placeholder_accelerator = u'Acelerador'
     form_placeholder_company_id = u'ID de compañía'
+    form_placeholder_paste_json = u'Lugar para insertar JSON'
+    form_placeholder_platform_name = u'Nombre de plataforma'
+    form_placeholder_platform_url = u'URL de plataforma'
+    form_placeholder_username = u'Nombre de usuario'
+    form_placeholder_password = u'Contraseña'
     form_checkbox_features = u'Características'
     form_checkbox_skip_auction = u'Saltar subasta'
     form_checkbox_documents_for_tender = u'Documentos de licitación'
     form_checkbox_documents_for_bids = u'Documentos de apuestas'
     form_checkbox_rent = u'Alquiler'
     form_checkbox_one_bid = u'Una apuesta'
+    form_checkbox_insert_json = u'Insertar JSON'
+    form_checkbox_get_json_from_cdb = u'Obtener JSON de BCD'
 
     # Buttons
     button_create_tender = u'Crear licitación'
@@ -182,19 +253,44 @@ class ES(object):
     button_delete_alerts = u'Eliminar mensajes'
     button_show_bids = u'Mostrar apuestas'
     button_add_to_company = u'Añadir'
+    button_add = u'Añadir'
+    button_save = u'Guardar'
+    button_format_json = u'Formatear'
+    button_get_json = u'Obtener JSON'
+    button_delete_tenders = u'Eliminar licitaciones'
 
     # Sections names
     section_create_tender_response = u'Resultados de las solicitudes'
     section_list_of_bids = u'Apuestas'
+    section_input_json = u'JSON original'
+    section_output_json = u'JSON formateado'
+    section_list_of_platforms = u'Lista de plataformas'
+    section_list_of_users = u'Lista de usuarios'
+    section_list_of_tenders = u'Lista de licitaciones'
+    section_popular_links = u'Enlaces populares'
 
     # Labels
     label_company_identifier = u'Identificador de compañía'
     label_company_id = u'ID de compañía'
     label_platform = u'Plataforma'
+    label_id_in_cdb = u'ID en BCD'
+    label_procurement_method_type = u'Tipo de licitación'
+    label_number_of_lots = u'Núm. de lotes'
+    label_creator = u'Creador'
+    label_api_version = u'Versión de API'
+    label_status = u'Estado'
+    label_added_to_site = u'En plataforma'
+    label_phone = u'Teléfono'
 
     # Messages
     message_tender_has_no_bids = u'No hay apuestas para esta licitación en la base de datos'
     message_auction_has_no_bids = u'No hay apuestas para esta subasta en la base de datos'
+    message_rendered_json_will_appear_here = u'JSON formateado aparecerá aquí'
+    message_empty_list = u'La lista no contiene elementos'
+    message_all_rights_reserved = u'Todos los derechos reservados'
+
+    # Hints
+    hint_delete = u'Borrar'
 
 
 class Translations(object):
@@ -296,6 +392,21 @@ class Translations(object):
     def form_placeholder_company_id(self):
         return self.class_selector().form_placeholder_company_id
 
+    def form_placeholder_paste_json(self):
+        return self.class_selector().form_placeholder_paste_json
+
+    def form_placeholder_platform_name(self):
+        return self.class_selector().form_placeholder_platform_name
+
+    def form_placeholder_platform_url(self):
+        return self.class_selector().form_placeholder_platform_url
+
+    def form_placeholder_username(self):
+        return self.class_selector().form_placeholder_username
+
+    def form_placeholder_password(self):
+        return self.class_selector().form_placeholder_password
+
     def form_checkbox_features(self):
         return self.class_selector().form_checkbox_features
 
@@ -314,6 +425,12 @@ class Translations(object):
     def form_checkbox_one_bid(self):
         return self.class_selector().form_checkbox_one_bid
 
+    def form_checkbox_insert_json(self):
+        return self.class_selector().form_checkbox_insert_json
+
+    def form_checkbox_get_json_from_cdb(self):
+        return self.class_selector().form_checkbox_get_json_from_cdb
+
     # BUTTONS
     def button_create_tender(self):
         return self.class_selector().button_create_tender
@@ -330,12 +447,45 @@ class Translations(object):
     def button_add_to_company(self):
         return self.class_selector().button_add_to_company
 
-    # NAMES FOR SECTIONS
+    def button_add(self):
+        return self.class_selector().button_add
+
+    def button_save(self):
+        return self.class_selector().button_save
+
+    def button_format_json(self):
+        return self.class_selector().button_format_json
+
+    def button_get_json(self):
+        return self.class_selector().button_get_json
+
+    def button_delete_tenders(self):
+        return self.class_selector().button_delete_tenders
+
+    # SECTIONS
     def section_create_tender_response(self):
         return self.class_selector().section_create_tender_response
 
     def section_list_of_bids(self):
         return self.class_selector().section_list_of_bids
+
+    def section_input_json(self):
+        return self.class_selector().section_input_json
+
+    def section_output_json(self):
+        return self.class_selector().section_output_json
+
+    def section_list_of_platforms(self):
+        return self.class_selector().section_list_of_platforms
+
+    def section_list_of_users(self):
+        return self.class_selector().section_list_of_users
+
+    def section_list_of_tenders(self):
+        return self.class_selector().section_list_of_tenders
+
+    def section_popular_links(self):
+        return self.class_selector().section_popular_links
 
     # LABELS
     def label_company_identifier(self):
@@ -347,9 +497,46 @@ class Translations(object):
     def label_platform(self):
         return self.class_selector().label_platform
 
+    def label_id_in_cdb(self):
+        return self.class_selector().label_id_in_cdb
+
+    def label_procurement_method_type(self):
+        return self.class_selector().label_procurement_method_type
+
+    def label_number_of_lots(self):
+        return self.class_selector().label_number_of_lots
+
+    def label_creator(self):
+        return self.class_selector().label_creator
+
+    def label_api_version(self):
+        return self.class_selector().label_api_version
+
+    def label_status(self):
+        return self.class_selector().label_status
+
+    def label_added_to_site(self):
+        return self.class_selector().label_added_to_site
+
+    def label_phone(self):
+        return self.class_selector().label_phone
+
     # MESSAGES
     def message_tender_has_no_bids(self):
         return self.class_selector().message_tender_has_no_bids
 
     def message_auction_has_no_bids(self):
         return self.class_selector().message_auction_has_no_bids
+
+    def message_rendered_json_will_appear_here(self):
+        return self.class_selector().message_rendered_json_will_appear_here
+
+    def message_empty_list(self):
+        return self.class_selector().message_empty_list
+
+    def message_all_rights_reserved(self):
+        return self.class_selector().message_all_rights_reserved
+
+    # HINTS
+    def hint_delete(self):
+        return self.class_selector().hint_delete
