cada1edeb2f4624aa7e72551e18dc4f92a454729
Andrii <buymistrandrey@gmail.com>
2018-07-31 01:59:20 +0300

    Add icons on reports list page

--- admin/pages.py
+++ admin/pages.py
@@ -9,12 +9,14 @@ class AdminPages:
 
     @staticmethod
     def page_admin_platforms():  # generate page with list of platforms for admin
-        content = render_template('admin/platforms.html', platforms=core.get_list_of_platforms(False), platform_roles=core.get_list_of_platform_roles())
+        content = render_template('admin/platforms.html', platforms=core.get_list_of_platforms(False),
+                                  platform_roles=core.get_list_of_platform_roles())
         return render_template('index.html', content=content)
 
     @staticmethod
     def page_admin_users(session):  # generate page with list of user for admin
-        content = render_template('admin/users.html', users=core.get_list_of_users(), user_roles=core.get_list_of_user_roles(),
+        content = render_template('admin/users.html', users=core.get_list_of_users(),
+                                  user_roles=core.get_list_of_user_roles(),
                                   super_user_flag=session['super_user'])
         return render_template('index.html', content=content)
 
@@ -30,12 +32,16 @@ class AdminPages:
 
     @staticmethod
     def page_admin_reports():  # generate page with list of user for admin
-        content = render_template('admin/reports.html', reports=core.get_list_of_reports())
+        content = render_template('admin/reports.html', report_types=core.get_list_of_report_types(),
+                                  report_priorities=core.get_list_of_report_priorities(),
+                                  reports=core.get_list_of_reports())
         return render_template('index.html', content=content)
 
     @staticmethod
     def page_admin_report_view(report_id):
-        content = render_template('report_view.html', report=core.get_report_info(report_id), report_types=core.get_list_of_report_types(),
-                                  report_status=core.get_list_of_report_statuses(), report_priorities=core.get_list_of_report_priorities(),
+        content = render_template('report_view.html', report=core.get_report_info(report_id),
+                                  report_types=core.get_list_of_report_types(),
+                                  report_status=core.get_list_of_report_statuses(),
+                                  report_priorities=core.get_list_of_report_priorities(),
                                   report_documents=core.get_report_documents(report_id))
         return render_template('index.html', content=content)
